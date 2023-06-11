9b86d338ff835b5d64e04da8c8d695292b9f34ae
UgurAdnann <adnann353@gmail.com>
2023-06-11 17:37:44 +0300

    initial settings done

---
+++ Assets/Art/Sprites/PlayerManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    #region Variables for General
+    #endregion
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
